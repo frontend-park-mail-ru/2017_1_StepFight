7a610e8e1fef9557bc6a1d080be9f7911e221ee9
denis <dondiego4697@mail.ru>
2017-04-22 02:27:16 +0300

    test service-worker

--- src/js/support/service-worker/service-worker.js
+++ src/js/support/service-worker/service-worker.js
@@ -4,37 +4,40 @@
 
 const CACHE_NAME = 'stepfight_serviceworker_v_1';
 
-const cacheUrls = [
+const CACHE_URLS = [
     '/',
     '/dist/app.bundle.css',
     '/dist/app.bundle.js'
 ];
 
-self.addEventListener('install', function (event) {
+self.addEventListener('install', (event) => {
     // задержим обработку события
     // если произойдёт ошибка, serviceWorker не установится
     event.waitUntil(
         // находим в глобальном хранилище Cache-объект с нашим именем
         // если такого не существует, то он будет создан
-        caches.open(CACHE_NAME).then(function (cache) {
+        caches.open(CACHE_NAME).then((cache) => {
             // загружаем в наш cache необходимые файлы
             console.warn('install!!');
-            return cache.addAll(cacheUrls);
-        })
-    )
+            return cache.addAll(CACHE_URLS);
+        }).then(self.skipWaiting())
+    );
 });
 
-self.addEventListener('fetch', function (event) {
+self.addEventListener('fetch', (event) => {
     console.warn('fetch!!');
-    event.respondWait(
+
+    event.respondWith(
         // ищем запрашиваемый ресурс в хранилище кэша
-        caches.match(event.request).then(function (cachedResponse) {
+        caches.match(event.request).then(function(cachedResponse) {
+
+            // выдаём кэш, если он есть
             if (cachedResponse) {
                 return cachedResponse;
             }
-            console.warn('request bitch!');
-            console.warn(event.request);
-            return fetch(event.request)
+
+            // иначе запрашиваем из сети как обычно
+            return fetch(event.request);
         })
-    )
+    );
 });
